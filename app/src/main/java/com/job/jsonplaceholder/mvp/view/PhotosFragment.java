564fe1b6e52453ff57438107bf0abb2be846039e
Vladislav <slavayus@gmail.com>
2018-06-19 22:52:40 +0300

    some changes. Disabled animation in the recycler view in the photos fragment. Shows user name in the toolbar in the photos fragment. Add a back button to the toolbar in the photos fragment.

--- app/src/main/java/com/job/jsonplaceholder/mvp/view/PhotosFragment.java
+++ app/src/main/java/com/job/jsonplaceholder/mvp/view/PhotosFragment.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SimpleItemAnimator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -45,10 +48,18 @@ public class PhotosFragment extends Fragment implements PhotosFragmentContractVi
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photos, container, false);
 
+        if (getActivity() != null) {
+            ActionBar supportActionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+            if (supportActionBar!=null) {
+                supportActionBar.setTitle(user.getName());
+            }
+        }
+
         RecyclerView mRecyclerView = view.findViewById(R.id.recycler_view_photos);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         mPhotosAdapter = new PhotosAdapter(this);
         mRecyclerView.setAdapter(mPhotosAdapter);
+        ((SimpleItemAnimator) mRecyclerView.getItemAnimator()).setSupportsChangeAnimations(false);
 
         mPresenter = new PhotosFragmentPresenter(new PhotosFragmentModel(getContext()));
         mPresenter.attachView(this);
